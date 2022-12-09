95a1534c8d5ea1a72fe0858b6941991e9da41049
Faizaan Khan <faizaankhan24@gmail.com>
2022-12-08 21:30:42 -0500

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Faizaan Khan on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
